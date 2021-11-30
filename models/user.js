0aed72d716059db32f93592d4581225fa6784337
Deadmau26 <26pkristen@gmail.com>
2021-11-30 18:45:36 +0530

    added db link

--- models/user.js
+++ models/user.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect("mongodb://localhost:27017/testDB", { useNewUrlParser: true }, { useUnifiedTopology: true }).then(() => {
+mongoose.connect("mongodb+srv://testuser:test1234@cluster0.lzems.mongodb.net/vaccine-data?retryWrites=true&w=majority", { useNewUrlParser: true }, { useUnifiedTopology: true }).then(() => {
   console.log('connection is successful');
 }).catch(error => {
   console.log('connection failed: ' + error);
